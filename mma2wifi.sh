ba8d057dcb2b38cc591ff6aa1822d73e6aefa59f
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2018-11-07 20:15:57 +0100

    bash files

---
+++ mma2wifi.sh
@@ -0,0 +1,26 @@
+
+i=1
+while [ $i -lt 4 ]
+do
+sleep 0.2
+echo 'Hacking wifi password for mma2wifi: ' $i '%'
+i=$((i + 1))
+done
+
+j=0
+while [ $j -lt 3 ]
+do
+sleep 1
+echo '.'
+j=$((j + 1))
+done
+
+k=0
+while [ $k -lt 2 ]
+do
+sleep 0.6
+echo 'processing'
+k=$((k + 1))
+done
+
+echo 'Password is: Lx612!w?sq22LE'
---
+++ while.sh
@@ -0,0 +1,7 @@
+
+i=0
+while [ $i -lt 4 ]
+do
+echo $i
+i=$((i + 1))
+done
